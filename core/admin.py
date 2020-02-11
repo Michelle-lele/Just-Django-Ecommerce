1c42dc811ccdadf09d7ce6ff091e280c1ff24a7e
Michelle-lele <fia_m@abv.bg>
2020-02-11 23:24:01 +0200

    Part 1 - Setup and project configuration v.1

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, orderItem, Order
+
+admin.site.register(Item)
+admin.site.register(orderItem)
+admin.site.register(Order)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+	title = models.CharField(max_length=100)
+	price = models.FloatField()
+
+	def _str__(self):
+		return self.title
+
+class orderItem(models.Model):
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	
+	def _str__(self):
+		pass
+
+class Order(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE)
+	items = models.ManyToManyField(orderItem)
+	start_date = models.DateTimeField(auto_now_add=True)
+	order_date = models.DateTimeField()
+	ordered = models.BooleanField(default=False)
+
+	def _str__(self):
+		return self.user.username
